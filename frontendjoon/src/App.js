95f766357d6a069fc96526ea9aa6772f21f24906
kangjoonseo <102732053+kangjoonseo@users.noreply.github.com>
2023-05-26 17:16:07 +0900

    Add files via upload

--- frontendjoon/src/App.js
+++ frontendjoon/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from './components/NavbarComp'
-import Home from './components/Home'
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
